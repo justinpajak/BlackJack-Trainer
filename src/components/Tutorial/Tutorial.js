0d657912ee302f0ec66adae704e24e65b8506078
justinpajak <jpajak@nd.edu>
2021-07-20 08:22:45 -0500

    removed uneccessary file

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,12 +1,21 @@
+import {useState} from 'react';
 import {Redirect} from 'react-router-dom';
 import "../../styles/Tutorial.css";
 
 const Tutorial = ({loggedIn}) => {
+    
+    const [showCouting, setShowCounting] = useState(true);
+    const [showTraining, setShowTraining] = useState(false);
+
+    const clickCountingB = () => {
+        console.log("clicked");
+    }
+
     return (
         <div>
             {loggedIn
             ? <div className="tutorial">
-                <h1>Tutorial</h1>
+                <h1 className="title">Tutorial</h1>
               </div>
             : <Redirect to="/auth"/>
             }
